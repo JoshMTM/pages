5dead11743b6be11470132f654f18e907ede3f55
Joshua Mitima <joshuamitima@joshuas-mbp.home>
2022-09-22 13:16:08 +0200

    ngContainerDirectives added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -69,4 +69,8 @@ export class AppComponent {
       url: 'https://images.unsplash.com/photo-1524749292158-7540c2494485?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80'
     },
   ]
+
+  checkWindowIndex(index: number) {
+    return Math.abs(this.currentPage - index) < 5;
+  }
 }
